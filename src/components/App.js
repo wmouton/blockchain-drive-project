fa341560d6f7038a69bc1011db00721a197f6849
l33th <l33th@protonmail.ch>
2021-12-28 14:07:01 +0200

    added form and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import DStorage from '../abis/DStorage.json'
+import DStorage from '../abis/DStorage.json';
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Main from './Main';
@@ -6,6 +6,9 @@ import Web3 from 'web3';
 import './App.css';
 
 //Declare IPFS
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
+
 
 class App extends Component {
   async componentWillMount() {
@@ -74,7 +77,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      account: '',
+      dstorage: null,
+      files: [],
       loading: false,
+      type: null,
+      name: null,
     };
 
     //Bind functions
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
 import { convertBytes } from './helpers';
-import moment from 'moment'
+import moment from 'moment';
 
 class Main extends Component {
-
   render() {
     return (
-      <div className="container-fluid mt-5 text-center">
-        <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
-            <div className="content">
+      <div className='container-fluid mt-5 text-center'>
+        <div className='row'>
+          <main
+            role='main'
+            className='col-lg-12 ml-auto mr-auto'
+            style={{ maxWidth: '1024px' }}
+          >
+            <div className='content'>
               <p>&nbsp;</p>
-              <h1>L33th Decentralized Storage</h1>
-              {/* Creatining uploading card ... */}
-                {/* Uploading file... */}
+              <div style={{ marginBottom: '50px', textAlign: 'center' }}>
+                <h1>L33th Decentralized Storage</h1>
+                <h6 style={{ textAlign: 'center' }}>by WMouton</h6>
+              </div>
+
+              <div
+                className='card mb-3 mx-auto bg-dark'
+                style={{ maxWidth: '512px' }}
+              >
+                <h2 className='text-white text-monospace bg-dark'>
+                  <b>
+                    <ins>Share File</ins>
+                  </b>
+                </h2>
+                <form
+                  onSubmit={event => {
+                    event.preventDefault();
+                    const description = this.fileDescription.value;
+                    this.props.uploadFile(description);
+                  }}
+                >
+                  <div className='form-group'>
+                    <br></br>
+                    <input
+                      id='fileDescription'
+                      type='text'
+                      ref={input => {
+                        this.fileDescription = input;
+                      }}
+                      className='form-control text-monospace'
+                      placeholder='description...'
+                      required
+                    />
+                  </div>
+                  <input
+                    type='file'
+                    onChange={this.props.captureFile}
+                    className='text-white text-monospace'
+                  />
+                  <button type='submit' className='btn-primary btn-block'>
+                    <b>Upload!</b>
+                  </button>
+                </form>
+              </div>
+
               <p>&nbsp;</p>
               {/* Create Table*/}
-              <table className="table-sm table-bordered text-monospace" style={{ width: '1000px', maxHeight: '450px'}}>
-              {/* Set table columns */}
+              <table
+                className='table-sm table-bordered text-monospace'
+                style={{ width: '1000px', maxHeight: '450px' }}
+              >
+                {/* Set table columns */}
                 {/* Mapping rows... */}
               </table>
             </div>
