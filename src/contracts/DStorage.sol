cafa641feabe4913a04bb5862e5ece91b98f65ad
l33th <l33th@protonmail.ch>
2021-12-28 12:16:44 +0200

    checking existence of file hash, type, description, name and size

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -30,20 +30,18 @@ contract DStorage {
   // Upload File function
   function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
 
-    
-
     // Make sure the file hash exists
-
+    require(bytes(_fileHash).length > 0);
     // Make sure file type exists
-
+    require(bytes(_fileType).length > 0);
     // Make sure file description exists
-
+    require(bytes(_fileDescription).length > 0);
     // Make sure file fileName exists
-
+    require(bytes(_fileName).length > 0);
     // Make sure uploader address exists
-
+    require(msg.sender!=address(0));
     // Make sure file size is more than 0
-
+    require(_fileSize>0);
 
     // Increment file id
     fileCount ++;
