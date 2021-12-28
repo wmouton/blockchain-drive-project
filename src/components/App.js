f7e935ce29682926ba2fbe82b0b1dfc6cce5fc3f
l33th <l33th@protonmail.ch>
2021-12-28 12:57:15 +0200

    client side - display account

--- src/components/App.js
+++ src/components/App.js
@@ -1,78 +1,70 @@
 //import DStorage from '../abis/DStorage.json'
 import React, { Component } from 'react';
-import Navbar from './Navbar'
-import Main from './Main'
+import Navbar from './Navbar';
+import Main from './Main';
 import Web3 from 'web3';
 import './App.css';
 
 //Declare IPFS
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   // Setting up web3
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
     }
   }
 
   async loadBlockchainData() {
     //Declare Web3
-
+    const web3 = window.web3;
+    
     //Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
 
     //Network ID
 
     //IF got connection, get data from contracts
-      //Assign contract
+    //Assign contract
 
-      //Get files amount
+    //Get files amount
 
-      //Load files&sort by the newest
+    //Load files&sort by the newest
 
     //Else
-      //alert Error
-
+    //alert Error
   }
 
   // Get file from user
-  captureFile = event => {
-  }
-
+  captureFile = event => {};
 
   //Upload File
   uploadFile = description => {
-
     //Add file to the IPFS
-
-      //Check If error
-        //Return error
-
-      //Set state to loading
-
-      //Assign value for the file without extension
-
-      //Call smart contract uploadFile function 
-
-  }
+    //Check If error
+    //Return error
+    //Set state to loading
+    //Assign value for the file without extension
+    //Call smart contract uploadFile function
+  };
 
   //Set states
   constructor(props) {
-    super(props)
-    this.state = {
-    }
+    super(props);
+    this.state = {};
 
     //Bind functions
   }
@@ -81,14 +73,17 @@ class App extends Component {
     return (
       <div>
         <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-              files={this.state.files}
-              captureFile={this.captureFile}
-              uploadFile={this.uploadFile}
-            />
-        }
+        {this.state.loading ? (
+          <div id='loader' className='text-center mt-5'>
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Main
+            files={this.state.files}
+            captureFile={this.captureFile}
+            uploadFile={this.uploadFile}
+          />
+        )}
       </div>
     );
   }
