c80f8116b4a4f7841570168312949903ff416622
l33th <l33th@protonmail.ch>
2021-12-28 14:59:22 +0200

    display uploaded files

--- src/components/Main.js
+++ src/components/Main.js
@@ -65,12 +65,79 @@ class Main extends Component {
 
               <p>&nbsp;</p>
               {/* Create Table*/}
+
               <table
                 className='table-sm table-bordered text-monospace'
                 style={{ width: '1000px', maxHeight: '450px' }}
               >
-                {/* Set table columns */}
-                {/* Mapping rows... */}
+                <thead style={{ fontSize: '15px' }}>
+                  <tr className='bg-dark text-white'>
+                    <th scope='col' style={{ width: '10px' }}>
+                      id
+                    </th>
+                    <th scope='col' style={{ width: '200px' }}>
+                      name
+                    </th>
+                    <th scope='col' style={{ width: '230px' }}>
+                      description
+                    </th>
+                    <th scope='col' style={{ width: '120px' }}>
+                      type
+                    </th>
+                    <th scope='col' style={{ width: '90px' }}>
+                      size
+                    </th>
+                    <th scope='col' style={{ width: '90px' }}>
+                      date
+                    </th>
+                    <th scope='col' style={{ width: '120px' }}>
+                      uploader/view
+                    </th>
+                    <th scope='col' style={{ width: '120px' }}>
+                      hash/view/get
+                    </th>
+                  </tr>
+                </thead>
+                {this.props.files.map((file, key) => {
+                  return (
+                    <thead style={{ fontSize: '12px' }} key={key}>
+                      <tr>
+                        <td>{file.fileId}</td>
+                        <td>{file.fileName}</td>
+                        <td>{file.fileDescription}</td>
+                        <td>{file.fileType}</td>
+                        <td>{convertBytes(file.fileSize)}</td>
+                        <td>
+                          {moment
+                            .unix(file.uploadTime)
+                            .format('h:mm:ss A M/D/Y')}
+                        </td>
+                        <td>
+                          <a
+                            href={
+                              'https://etherscan.io/address/' + file.uploader
+                            }
+                            rel='noopener noreferrer'
+                            target='_blank'
+                          >
+                            {file.uploader.substring(0, 10)}...
+                          </a>
+                        </td>
+                        <td>
+                          <a
+                            href={
+                              'https://ipfs.infura.io/ipfs/' + file.fileHash
+                            }
+                            rel='noopener noreferrer'
+                            target='_blank'
+                          >
+                            {file.fileHash.substring(0, 10)}...
+                          </a>
+                        </td>
+                      </tr>
+                    </thead>
+                  );
+                })}
               </table>
             </div>
           </main>
